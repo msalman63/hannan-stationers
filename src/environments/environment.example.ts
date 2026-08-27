0dbd6f68cc188c65a5ade5fe80bc109b933bcb9e
msalman63 <connect.msalman@gmail.com>
2026-08-27 12:42:59 +0500

    fix: update supabase publishable key

--- src/environments/environment.example.ts
+++ src/environments/environment.example.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   supabaseUrl: 'YOUR_SUPABASE_URL_HERE',
-  supabaseKey: 'YOUR_SUPABASE_ANON_KEY_HERE'
-}
+  supabaseKey: 'YOUR_SUPABASE_PUBLISHABLE_KEY_HERE'
+};
